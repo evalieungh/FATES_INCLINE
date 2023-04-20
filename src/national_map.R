76639e16fb16e9d6a6f5bc415a0d64d8e21c351a
evalieungh <evaleriksen@gmail.com>
2023-04-20 21:26:38 +0200

    upload warmed forcing

--- src/national_map.R
+++ src/national_map.R
@@ -10,7 +10,6 @@ library(ggplot2)
 library(mapdata)
 library(raster)
 library(sf)
-theme_set(theme_bw()) # set ggplot theme
 
 # specify point coordinates
 alp4_coords <- data.frame(longitude=c(6.41504),latitude=c(60.9335))
@@ -21,10 +20,36 @@ europe <- ne_countries(scale='medium',
 											 returnclass = 'sf')
 
 # plot map with points for sites
-ggplot(data = europe) + 
-		geom_sf(fill = "darkgrey") + 
-		geom_point(data = alp4_coords, 
-							 aes(x = longitude, y = latitude), 
-							 size = 1, shape = 19, color = "red") +
-		coord_sf(xlim = c(-5,25), ylim = c(50,65))+
-  	xlab('') + ylab('')
+fig1_map <- ggplot(data = europe) +
+  geom_sf(fill = "darkgrey",
+          size = 1) +
+  geom_point(
+    data = alp4_coords,
+    aes(x = longitude, y = latitude),
+    size = 1,
+    shape = 19,
+    color = "red"
+  ) +
+  coord_sf(xlim = c(-6, 23),
+           ylim = c(45, 70)) +
+  xlab('') + ylab('') +
+  theme_minimal() +
+  theme(axis.text = element_blank()
+  #  axis.text.x = element_text(
+  #    size = 8,
+  #    angle = 270,
+  #    hjust = 0,
+  #    vjust = 0.5
+  #  ),
+  #  axis.text.y = element_text(size = 8),
+  )
+fig1_map
+
+# save the figure
+ggsave("../results/figures/fig1_map.jpeg",
+       plot = fig1_map,
+       device = "jpeg",
+       width = 500, 
+       height = 750,
+       dpi = "print",
+       units = "px")
